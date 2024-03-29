a00aaa85e429cfba703f49f39ef0f9608b654d5d
Asadi karthik <155179825+asadikarthik@users.noreply.github.com>
2024-03-29 16:02:19 +0530

    Patterns in java

--- java codes/pattern4.java
+++ java codes/pattern4.java
@@ -1,34 +1,32 @@
-import java.util.Scanner;
-public class pattern4
+class pattern4
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        Scanner s = new Scanner(System.in);
-        int n =s.nextInt();
+        int n=8;
+        
         for(int i=1;i<=n;i++)
         {
-            for(int z=1;z<=n-i;z++)
+            for(int j=1;j<=n-i;j++)
             {
                 System.out.print(" ");
             }
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=2*i-1;j++)
             {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
-        for(int i=n;i>0;i--)//5
+        for(int i=n-1;i>=1;i--)
         {
-            for(int z=n;z<=n-i;z++)//z= 5,
+            for(int j=1;j<=n-i;j++)
             {
                 System.out.print(" ");
             }
-            for(int j=i;j>0;j--)
+            for(int j=1;j<=2*i-1;j++)
             {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
-        s.close();
     }
 }
